75052b186525277a04eb2628976bfbdf96649dd5
komainu-ya <app.komainu.ya@gmail.com>
2024-04-21 16:54:47 +0900

    Create Project

---
+++ ManualSplash/ManualSplash/ManualSplashApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManualSplashApp.swift
+//  ManualSplash
+//
+//  Created by komainu-ya on 2024/04/21.
+//
+
+import SwiftUI
+
+@main
+struct ManualSplashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
