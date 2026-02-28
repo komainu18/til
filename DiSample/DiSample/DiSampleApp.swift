1761f183a1311316d4c6ba4e733d213e2d716ee3
komainu-ya <app.komainu.ya@gmail.com>
2026-03-01 04:26:32 +0900

    public化

--- DiSample/DiSample/DiSampleApp.swift
+++ DiSample/DiSample/DiSampleApp.swift
@@ -6,9 +6,17 @@
 //
 
 import SwiftUI
+import FeatureModule
+//import InterfaceModule
+//import ImplementationModule
 
 @main
 struct DiSampleApp: App {
+    init() {
+//        prepareDependencies {
+//            $0.messageRepository = .live
+//        }
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
